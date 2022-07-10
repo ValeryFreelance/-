4e9ab6200159ad808f5914c5d382b647630f3fc6
Valery <108897657+ValeryFreelance@users.noreply.github.com>
2022-07-11 00:19:34 +0300

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите пункт");
+            System.out.println("Сложение");
+            System.out.println("Выберите пункт");
+            System.out.println("Выберите пункт");
+            System.out.println("Выберите пункт");
+            System.out.println("Выберите пункт");
+        }
+    }
+
+}
+
